6c8383f54513b648dad7925ef2185dee0142cdfe
jsy3831 <61769145+jsy3831@users.noreply.github.com>
2023-04-20 15:14:08 +0900

    Time: 0 ms, Memory: 41 MB - LeetHub

--- powx-n/powx-n.java
+++ powx-n/powx-n.java
@@ -1,15 +1,30 @@
 class Solution {
     public double myPow(double x, int n) {
         if (n == 0)
-			return 1;
+            return 1;
+        else if (x == 0 || x == 1 || n == 1)
+            return x;
 
-		if (x == 0 || x == 1 || n == 1)
-			return x;
+        if (n == Integer.MIN_VALUE) {
+            x *= x;
+            n /= 2;
+        }
 
-		if (n < 0) {
-			return (1 / x) * myPow(1 / x, -(n + 1));
-		}
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
 
-		return (n % 2 == 0) ? myPow(x * x, n / 2) : x * myPow(x * x, n / 2);
+        double ans = 1.0;
+        
+        while (n > 0) {
+            if (n % 2 == 1)
+                ans *= x;
+            
+            x *= x;
+            n /= 2;
+        }
+
+        return ans;
     }
 }
